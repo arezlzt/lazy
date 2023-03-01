ad09a38642ffb63baa54b8109beed23e4274a233
liutianzheng <liutianzheng@infocq.com>
2023-03-01 10:45:05 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # lazy
 The go tools 自己整理的常用的工具类
+#### 安装使用
+
+```go
+go get -v -u github.com/areslzt/lazy
+
+import (
+    "github.com/areslzt/lazy"
+)
+```
---
+++ aes/aes_test.go
@@ -0,0 +1,33 @@
+package aes
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+const (
+	key = "bGcGfWb3Kg2s4gcG"
+	iv  = "aebksHkG4jAEk2Ag"
+)
+
+func TestEncrypt(t *testing.T) {
+	expect := "DIVKKtNuw62mZx4GO3mdHQ=="
+	actual, err := Encrypt("hello world", []byte(key), iv)
+	if err != nil {
+		assert.Error(t, err)
+	} else {
+		assert.Equal(t, expect, actual)
+	}
+
+}
+func TestDecrypt(t *testing.T) {
+	expect := "hello world"
+	actual, err := Decrypt("DIVKKtNuw62mZx4GO3mdHQ==", []byte(key), iv)
+	if err != nil {
+		assert.Error(t, err)
+	} else {
+		assert.Equal(t, expect, actual)
+	}
+	time.Now()
+}
